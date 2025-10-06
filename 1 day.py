1b08a3b36cd21fedd1034c1bc1fd9639ee8a5962
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2025-10-06 09:38:35 +0530

    Added new folder 'patterns' with alphabet pattern program

---
+++ 1 day.py
@@ -0,0 +1,78 @@
+
+# 1 st
+
+
+# n=int(input("n"))
+# for i in range(1,n+1,1):
+#     for j in range(1,n-1+1,1):
+#         if i==1 or i==n or j==1 or j==n-1:
+#             print("*",end='')
+#         else:
+#             print(" ",end="")
+#     print("")
+ 
+# 2nd
+# n=4
+# for i in range(1,4+1,1):
+#     for j in range(1,n-i+1,1):
+#         print(" ",end="")
+#     for j in range(1,i+1,1):
+#         print(i,end=" ")
+#     print(" ")
+
+
+ 
+ # 3rd programm
+ 
+ 
+ 
+# n=4
+# for i in range(1,4+1,1):
+#     for j in range(1,i+1,1):
+#         print(j,end='')
+#     print(" ")
+
+
+#4th
+
+# n=4
+# for i in range(1,4+1,1):
+#     for j in range(1,n-i+1+1,1):
+#         print(j,end="")
+#     print(" ")
+
+
+
+#5th 
+
+# n=4
+# count=1
+# for i in range(1,4+1,1):
+#     for j in range(1,i+1,1):
+#         print(count,end="")
+#         count+=1
+#     print(" ")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    
+
+    
