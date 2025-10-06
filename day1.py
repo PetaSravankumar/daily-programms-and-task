166732d5554bcc5e7f33fa04d315d5a7e7b30e7e
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2025-10-06 09:44:39 +0530

    Added new folder 'patterns' with alphabet pattern program

--- day1.py
+++ day1.py
@@ -24,21 +24,21 @@
 
 #4rd program
 
-n=7
-for i in range(1,7+1,1):
-    for j in range(1,n-i+1+1,1):
-        print("*",end=" ")
-    print("")
+# n=7
+# for i in range(1,7+1,1):
+#     for j in range(1,n-i+1+1,1):
+#         print("*",end=" ")
+#     print("")
 
 #5th programm
 
-# n=7
-# a="A"
-# for i in range(1,7+1,1):
-#     for j in range(1,i+1,1):
-#         c=chr(ord(a)+j-1)
-#         print(c,end=" ")
-#     print("")
+n=7
+a="A"
+for i in range(1,7+1,1):
+    for j in range(1,i+1,1):
+        c=chr(ord(a)+j-1)
+        print(c,end=" ")
+    print("")
 
 #6th programm
 
