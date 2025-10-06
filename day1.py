30ed1b5e0b15b07101b4e8ac25d83113da6896d3
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2025-10-06 09:43:53 +0530

    Added new folder 'patterns' with alphabet pattern program

--- day1.py
+++ day1.py
@@ -8,8 +8,86 @@
 
 #2nd  programm
 
+# n=7
+# for i in range(1,7+1,1):
+#     for j in range(1,n-i+1+1,1):
+#         print(j,end=" ")
+#     print("")
+
+#3rd programm
+
 n=7
 for i in range(1,7+1,1):
-    for j in range(1,n-i+1+1,1):
-        print(j,end=" ")
+    for j in range(1,i+1,1):
+        print("*",end=" ")
     print("")
+
+#4rd program
+
+# n=7
+# for i in range(1,7+1,1):
+#     for j in range(1,n-i+1+1,1):
+#         print("*",end=" ")
+#     print("")
+
+#5th programm
+
+# n=7
+# a="A"
+# for i in range(1,7+1,1):
+#     for j in range(1,i+1,1):
+#         c=chr(ord(a)+j-1)
+#         print(c,end=" ")
+#     print("")
+
+#6th programm
+
+# n=7
+# a='A'
+# for i in range(1,n+1,1):
+#     for j in range(1,n-i+1+1,1):
+#         c=chr(ord(a)+j-1)
+#         print(c,end=" ")
+#     print("")
+
+#7th programm
+# n=7
+# for i in range(1,n+1,1):
+#     for j in range(1,n-i+1,1):
+#         print(" ",end="")
+#     for j in range(1,i+1,1):
+#         print(j,end=" ")
+#     print("")
+
+#8th programm
+# n=7
+# for i in range(1,n+1,1):
+#     for j in range(1,i-1+1,1):
+#         print(" ",end="")
+#     for j in range(1,n-i+1+1,1):
+#         print(j,end=" ")
+#     print("")
+    
+#9 th programm
+
+# n=7
+# a="A"
+# for i in range(1,n+1,1):
+#     for j in range(1,n-i+1,1):
+#         print(" ",end="")
+#     for j in range(1,i+1,1):
+#         c=chr(ord(a)+i-1)
+#         print(c,end=" ")
+#     print("")
+
+#10 th programm
+
+# n=7
+# a="A"
+# for i in range(1,n+1,1):
+#     for j in range(1,i-1+1,1):
+#         print(" ",end="")
+#     for j in range(1,n-i+1+1,1):
+#         c=chr(ord(a)+j-1)
+#         print(c,end=" ")
+#     print("")
