491d08217a0574c369d0a1427571c3b18c5d2a3e
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2025-10-07 08:07:43 +0530

    Added new folder 'patterns' with alphabet pattern program

--- day2.py
+++ day2.py
@@ -7,22 +7,22 @@
 # print(count)
 
 #2nd programm
-# s1="sravan"
-# s2="sraven"
-# c1=0
-# c2=0
-# for i in s1:
-#     c1+=1
-# for i in s2:
-#     c2+=1
-# if c1!=c2:
-#     print("not equal")
-# else:
-#     same=True
-#     for i in range(c1):
-#         if s1[i]!=s2[i]:
-#             same=False
-#             break
+s1="sravan"
+s2="sraven"
+c1=0
+c2=0
+for i in s1:
+    c1+=1
+for i in s2:
+    c2+=1
+if c1!=c2:
+    print("not equal")
+else:
+    same=True
+    for i in range(c1):
+        if s1[i]!=s2[i]:
+            same=False
+            break
 if same==True:
     print("equal")
 else:
