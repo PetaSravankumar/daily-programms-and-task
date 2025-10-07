89a71abffebfa336a564512f576a7922d9926da5
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2025-10-07 08:08:42 +0530

    Added new folder 'patterns' with alphabet pattern program

--- day2.py
+++ day2.py
@@ -1,60 +1,60 @@
-# 1st programm
+# # 1st programm
 
-s="sravan"
-count=0
-for i in s:
-    count+=1
-print(count)
-
-#2nd programm
-s1="sravan"
-s2="sraven"
-c1=0
-c2=0
-for i in s1:
-    c1+=1
-for i in s2:
-    c2+=1
-if c1!=c2:
-    print("not equal")
-else:
-    same=True
-    for i in range(c1):
-        if s1[i]!=s2[i]:
-            same=False
-            break
-if same==True:
-    print("equal")
-else:
-    print("not equal")
-
-#3rd progeamm
-
-# s=input("enter the string :")
-# rev=""
-# for i in range(len(s)-1,-1,-1):
-#     rev=rev+s[i]
-# print(rev)
-
-
-#4th programm
-
-# s=input("enter the string :")
-# rev=""
-# for i in range(len(s)-1,-1,-1):
-#     rev=rev+s[i]
-# if rev==s:
-#     print("palindrom")
-# else:
-#     print("not a palindrom")
-
-# 5th programm
-
-# s=input("enter the string :")
-# s1=""
+# s="sravan"
 # count=0
-# s1=s
+# for i in s:
+#     count+=1
+# print(count)
+
+# #2nd programm
+# s1="sravan"
+# s2="sraven"
+# c1=0
+# c2=0
 # for i in s1:
-#     if i==" ":
-#         count+=1
-# print(count+1)
+#     c1+=1
+# for i in s2:
+#     c2+=1
+# if c1!=c2:
+#     print("not equal")
+# else:
+#     same=True
+#     for i in range(c1):
+#         if s1[i]!=s2[i]:
+#             same=False
+#             break
+# if same==True:
+#     print("equal")
+# else:
+#     print("not equal")
+
+# #3rd progeamm
+
+# # s=input("enter the string :")
+# # rev=""
+# # for i in range(len(s)-1,-1,-1):
+# #     rev=rev+s[i]
+# # print(rev)
+
+
+# #4th programm
+
+# # s=input("enter the string :")
+# # rev=""
+# # for i in range(len(s)-1,-1,-1):
+# #     rev=rev+s[i]
+# # if rev==s:
+# #     print("palindrom")
+# # else:
+# #     print("not a palindrom")
+
+# # 5th programm
+
+# # s=input("enter the string :")
+# # s1=""
+# # count=0
+# # s1=s
+# # for i in s1:
+# #     if i==" ":
+# #         count+=1
+# # print(count+1)
